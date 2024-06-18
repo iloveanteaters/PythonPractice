d1a57c957a3557ba36c1e9f47c39776f4113deec
iloveanteaters <missafcjp@hotmail.com>
2024-06-18 14:02:16 +0100

    Test commit

---
+++ VariablesPractice.py
@@ -0,0 +1,28 @@
+# Variables temporarily store data, price of product, email etc
+# To start, you name your variable and add a value
+
+age = 26
+age = 30
+print(age)
+
+# Here I have made a variable named age, and assigned my age, I have then printed this into the console
+# When printing the variable I didn't use quotes as I wanted to print the value, not the word
+# Program is excecuted from top to bottom, you can change the variable, I have done this above and printed the new age
+
+price = 19.95
+
+# Python numbers can be decimal or whole
+# Using multipe words use underscore
+# Use quotes for a string as below
+
+first_name = "Amy"
+
+is_online = True
+
+# Boolean values are true or false,these should be capitalized
+
+#Input is asking a question in the terminal, and the name is assigning the answer to a variable
+name = input("What is your name?")
+print("Hello" + name)
+
+#Here I am combining strings, I type in my name and the console responds with "Hello Amy"
