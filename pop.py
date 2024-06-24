1a313e3c15029e2e1fe8d0d3ef16c272c0d0888f
Amy Prescott <168540003+iloveanteaters@users.noreply.github.com>
2024-06-24 14:11:51 +0100

    Create pop.py

---
+++ pop.py
@@ -0,0 +1,10 @@
+data_science_topics = ["Machine Learning", "SQL", "Pandas", "Algorithms", "Statistics", "Python 3"]
+print(data_science_topics)
+
+# Your code below: 
+removed_element = data_science_topics.pop()
+print(data_science_topics)
+print(removed_element)
+
+data_science_topics.pop(3)
+print(data_science_topics)
